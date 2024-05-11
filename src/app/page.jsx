00088096640582bfb7686fe72dca07d23a7be6f2
cheesely <chelynnfoster@gmail.com>
2024-05-10 21:31:44 -0700

    add b2b script

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import clsx from 'clsx'
 
+
 import { Button } from '@/components/Button'
 import { Card } from '@/components/Card'
 import { Container } from '@/components/Container'
